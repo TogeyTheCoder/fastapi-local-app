7fd73b25ef6e6cf87b75ab02f294cc1d906bb312
TogeyTheCoder <yeeting.yeetus.yeet@gmail.com>
2026-06-25 12:14:42 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+# Access app in http://127.0.0.1:8000
+# Root is still http://0.0.0.0:8000
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request":request, "backend_msg":"Hello from FastAPI!"}
+    )
